a4cc814c3cf0eca5bddc7b82c184b0890e6d06ce
randres-arcila <randres.arcila@udea.edu.co>
2019-07-29 21:41:33 -0500

    Ejercicio 2 corregido

--- ejercicio2/ejercicio2.c
+++ ejercicio2/ejercicio2.c
@@ -1,23 +1,28 @@
-#include<stdio.h>
-#include<error.h>
-#include<stdlib.h>
-#include<fcntl.h>
-int main(int argc, char *argv[]) {
-  int fd;
-  int pid;
-  char ch1, ch2;
-  fd = open("archivo.txt", O_RDWR);
-  read(fd, &ch1, 1);
-  printf("En el padre: ch1 = %c\n", ch1);
-  if ((pid = fork()) < 0) {
-    perror("fork fallo");
-    exit(-1); //Sale con código de error
-  } else if (pid == 0) {
-    read(fd, &ch2, 1);
-    printf("En el hijo: ch2 = %c\n", ch2);
-  } else {
-    read(fd, &ch1, 1);
-    printf("En el padre: ch1 = %c\n", ch1);
-  }
-  return 0;
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    int texto;
+    char *c;
+    int fd = open("archivo.txt", O_WRONLY | O_CREAT | O_TRUNC, 644);
+    int f = fork();
+
+    if (f == 0){
+        char a[] = "Soy el hijo\n\0";
+        texto = write(fd, a, strlen(a));
+        c = "Fin hijo\n\0";
+    }else{
+        char b[] = "Soy el padre\n\0";
+        texto = write(fd, b, strlen(b));
+        c = "Fin padre\n\0";
+    }
+    
+    texto = write(fd, c, strlen(c));
+    printf("Finalizando proceso. (File descriptor %d)\n", fd);
+
+    return 0;
 }
